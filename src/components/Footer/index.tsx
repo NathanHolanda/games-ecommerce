f55a822ca6f38845849703fff0d283acc9117b87
hl_nathan <50773888+NathanHolanda@users.noreply.github.com>
2022-04-15 19:22:14 -0300

    botão para o linkedin

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Text } from "@chakra-ui/react"
+import { Box, Icon, Text } from "@chakra-ui/react"
+import {AiOutlineLinkedin} from "react-icons/ai"
 
 function Footer(){
     return (
@@ -7,7 +8,7 @@ function Footer(){
           position="absolute"
           bottom={0}
           w="100%"
-          p="5"
+          p="4"
           maxWidth={1200}
           textAlign="center"
           borderTop="1px solid"
@@ -16,7 +17,16 @@ function Footer(){
             <Text fontSize="md" fontWeight="500">
                 Jogador Karo &copy; { new Date().getFullYear() }
             </Text>
-            <Text fontSize="xs" color="gray.500">Made by Nathan Holanda</Text>
+            <Text fontSize="xs" color="gray.500">
+                Made by Nathan Holanda
+            </Text>
+            <a target="_blank" href="https://linkedin.com/in/nathan-holanda-182a5916b" aria-label="Meu LinkedIn" title="Meu LinkedIn">
+                <Icon
+                    as={AiOutlineLinkedin}
+                    color="blue.500"
+                    fontSize="xl"
+                /> 
+            </a>
         </Box>
     )
 }
